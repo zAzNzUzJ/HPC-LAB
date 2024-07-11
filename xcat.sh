4eae6f7c3d7a0b364f2cfcbf6a69f1fe8b8207bf
Anuj M <61815166+zAzNzUzJ@users.noreply.github.com>
2024-07-12 00:57:04 +0530

    Update xcat.sh

--- xcat.sh
+++ xcat.sh
@@ -1,3 +1,7 @@
+ ONLY FOR STATEFULLL
+ 
+ 
+ 
  sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
  sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
